216d8d60ecb652e6b7efd1a1dd89330f6984d90c
wiselight <company@wiselight.kr>
2022-09-24 00:34:25 +0900

    2442

---
+++ 김현욱/1주차/2442.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n - (i + 1); j++)
+			cout << " ";
+		for(int j = 0; j < 2 * (i + 1) - 1; j++)
+			cout << "*";
+		cout << "\n";
+	}
+	
+	return 0;
+}
