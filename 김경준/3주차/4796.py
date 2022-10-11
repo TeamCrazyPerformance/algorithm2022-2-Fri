9c74e12f6ec49abe7ea85c47e8e519a5f24a9e42
김경준 <49617190+AzureSoda@users.noreply.github.com>
2022-10-12 00:09:18 +0900

    Create 4796.py

---
+++ 김경준/3주차/4796.py
@@ -0,0 +1,11 @@
+i = 0
+while True:
+    i+=1
+    l, p, v = map(int, input().split())
+    if l==0 and p==0 and v==0:
+        break
+    a = v//p
+    b = v%p
+    if l<b:
+        b = l
+    print("Case %d: %d" %(i, a*l+b))
