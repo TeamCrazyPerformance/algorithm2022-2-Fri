d759d43b2a02bd1c585000f1e2b4e6c2a915e378
김경준 <49617190+AzureSoda@users.noreply.github.com>
2022-11-04 16:23:01 +0900

    Update 2805.py

--- 김경준/4주차/2805.py
+++ 김경준/4주차/2805.py
@@ -1,5 +1,8 @@
 #2805
 
+import sys
+input = sys.stdin.readline
+
 n,m=map(int,input().split())
 trees=list(map(int,input().split()))
 start,end=1,sum(trees)
@@ -23,3 +26,11 @@ while start<=end:
         #끝위치
         end=mid-1
 print(end)        
+
+
+#pypy로 하면 더 빠르게 통과됨
+#PyPy3에서는 실행시, 자주 쓰이는 코드를 캐싱하는 기능이 있기 때문에 ,
+#즉  메모리를 조금 더 사용하여 그것들을 저장하고 있어, 실행속도를 개선할 수 있다는 것이기 때문에,
+#간단한 코드상에서는 Python3가 메모리, 속도 측에서 우세할 수 있는 것이고,
+#복잡한 코드(반복)을 사용하는 경우에서는 PyPy3가 우세하기 때문에
+#-> 코드 상황에 맞추어 두 구현체(PyPy3, Python3)를 적절하게 사용하는 것이 효율적이라고 할 수 있다.
