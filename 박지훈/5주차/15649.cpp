ec39089602319729b32dfbdd53b9024d2c598e87
pianop <kphjjh723@gmail.com>
2022-11-09 11:46:11 +0900

    15649.cpp 주석 추가

--- 박지훈/5주차/15649.cpp
+++ 박지훈/5주차/15649.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #define fastio ios::sync_with_stdio(false), cin.tie(NULL);
 using namespace std;
-typedef long long ll;
 typedef vector<int> vi;
 
 
@@ -25,13 +23,14 @@ private:
 	void init()
 	{
 		cin >> mN >> mM;
-		mSeq.resize(mM);
-		mCheck.resize(mN + 1);
+		mSeq.resize(mM);		// 길이가 M인 수열을 담을 배열
+		mCheck.resize(mN + 1);	// N개의 수 중 mSeq 배열에 들어가있는 수를 체크
 	}
 
-	void backTracking(int now)
+	void backTracking(int idx)
 	{
-		if (now == mM)
+		//	mSeq 배열에 M개의 수가 모였다면
+		if (idx == mM)
 		{
 			for (int num : mSeq)
 				cout << num << " ";
@@ -39,17 +38,17 @@ private:
 			return;
 		}
 
-		int next = now + 1;
+		int next = idx + 1;
 		for (int i = 1; i <= mN; i++)
 		{
-			if (mCheck[i] >= 0)
+			if (mCheck[i] == 0)
 			{
-				mCheck[i] = -1;
-				mSeq[now] = i;
+				mCheck[i] = 1;		
+				mSeq[idx] = i;		// mSeq의 현재 idx에 i를 넣어주고 i를 체크
 
 				backTracking(next);
 
-				mCheck[i] = 0;
+				mCheck[i] = 0;		// 이후의 탐색의 재귀에서 지금의 i가 쓰일 수 있어야 하므로 체크를 풀어줌
 			}
 		}
 	}
