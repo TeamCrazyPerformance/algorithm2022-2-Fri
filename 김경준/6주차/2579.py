d4186c0ede5eb2a75c972db7463dab63510129d8
김경준 <49617190+AzureSoda@users.noreply.github.com>
2022-11-18 17:02:17 +0900

    Add files via upload

---
+++ 김경준/6주차/2579.py
@@ -0,0 +1,16 @@
+#2579
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stair = [0 for i in range(301)] #계단 점수
+dp = [0 for i in range(301)]
+for i in range(n):
+    stair[i] = int(input())
+dp[0] = stair[0]
+dp[1] = stair[0] + stair[1]
+dp[2] = max(stair[1] + stair[2], stair[0] + stair[2])
+for i in range(3, n):
+    dp[i] = max(dp[i - 3] + stair[i - 1] + stair[i], dp[i - 2] + stair[i])
+print(dp[n - 1])
