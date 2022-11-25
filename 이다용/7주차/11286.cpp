828a60da94c55884d6d41d427440563076ff7959
eagleda <83822982+eagleda@users.noreply.github.com>
2022-11-25 13:08:52 +0900

    11286

---
+++ 이다용/7주차/11286.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include <utility>
+using namespace std;
+
+vector<long int> heap;
+int N;
+long int input;
+void fastIO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+}
+
+void init() {
+	cin >> N;
+	heap.push_back(0);
+}
+
+int main() {
+	fastIO();
+	init();
+	for (int i = 0; i < N; i++) {
+		cin >> input;
+		if (abs(input) > 0) { //insert
+			heap.push_back(input);
+			int pos = heap.size() - 1;
+			while (pos > 1 && (abs(heap[pos]) < abs(heap[pos / 2])||(abs(heap[pos]) == abs(heap[pos / 2])&&heap[pos]<heap[pos/2]))) {
+	
+				swap(heap[pos], heap[pos / 2]);
+
+				pos /= 2;
+			}
+		}
+		else { //delete
+			if (heap.size() - 1 < 1) {
+				cout << 0 << "\n";
+				continue;
+			}
+			cout << heap[1] << "\n";
+			swap(heap[1], heap[heap.size() - 1]);
+			heap.pop_back();
+			int pos = 1;
+			while (pos * 2 < heap.size()) {
+				long int smallChild = heap[pos * 2];
+				int smallChildPos = pos * 2;
+
+				if (smallChildPos + 1 < heap.size() && (abs(smallChild) > abs(heap[smallChildPos + 1])||(abs(smallChild)==abs(heap[smallChildPos+1])&&smallChild>heap[smallChildPos+1]))) {
+					smallChild = heap[pos * 2 + 1];
+					smallChildPos = pos * 2 + 1;
+				}
+				if ((abs(heap[pos]) < abs(heap[smallChildPos])) || (abs(smallChild) == abs(heap[pos]) && smallChild > heap[pos])) break;
+				swap(heap[pos], heap[smallChildPos]);
+				swap(pos, smallChildPos);
+			}
+
+		}
+	}
+}
