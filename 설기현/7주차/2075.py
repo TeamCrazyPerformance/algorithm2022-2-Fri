cef348bcdeb2372a73ed08a4692c782f8725c869
Genesis1:31 <83863024+seol731@users.noreply.github.com>
2022-11-25 19:19:38 +0900

    Update 2075.py

--- 설기현/7주차/2075.py
+++ 설기현/7주차/2075.py
@@ -1,1 +1,13 @@
-
+import sys, heapq 
+N = int(sys.stdin.readline())
+heap = []
+for i in range(N):
+    num = map(int,sys.stdin.readline().split())
+    for j in num:
+        if len(heap) < N:
+            heapq.heappush(heap,j)
+        else:
+            if j > heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, j)
+print(heap[0])
