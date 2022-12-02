9a9d97fde59ca9eaf49675a55beb1694826a5112
Genesis1:31 <83863024+seol731@users.noreply.github.com>
2022-12-02 17:41:01 +0900

    Update 24444.py

--- 설기현/8주차/24444.py
+++ 설기현/8주차/24444.py
@@ -1,4 +1,69 @@
-# 24444 알고리즘 수업 - 너비 우선 탐색 1
+# 24444 알고리즘 수업 - 너비 우선 탐색 1  - per1
+from collections import deque 
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9) 
+N, M, R = map(int, sys.stdin.readline().split())
+
+undirected_graph = [[]for i in range(N+1)]  # 0index이므로 N+1
+visited = [0]*(N+1)                         # 방문한 노드 표시하기 위해 
+
+for i in range(M):
+    u, v = map(int, sys.stdin.readline().split()) 
+    undirected_graph[u].append(v)
+    undirected_graph[v].append(u)
+cnt = 1 
+
+
+
+def bfs(R):
+    global cnt 
+    q = deque([R])
+    visited[R] = cnt     
+    while q:
+        ql = q.popleft()
+        undirected_graph[ql].sort()
+        for i in undirected_graph[ql]:
+            if visited[i] == 0:
+                cnt+=1
+                visited[i] = cnt
+                q.append(i)
+    print(*visited[1:], sep="\n")
+
+
+
+bfs(R) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 24444 알고리즘 수업 - 너비 우선 탐색 1 -per2
 #Python에서 Queue 자료구조는 Stack과 Queue를 합친 Deque을 사용. collections 라이브러리의 deque(double-ended queue의 약자)
 from collections import deque 
 import sys
