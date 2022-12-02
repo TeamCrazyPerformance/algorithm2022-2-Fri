5cddd1bbef57827e69515557228822ff832a666e
Genesis1:31 <83863024+seol731@users.noreply.github.com>
2022-12-02 18:15:21 +0900

    Update 2178.py

--- 설기현/8주차/2178.py
+++ 설기현/8주차/2178.py
@@ -46,3 +46,39 @@ dfs(0,0)
 
 
 ## 2178번 미로탐색 bfs방식 -----
+import sys 
+# sys.setrecursionlimit(10**9)
+from collections import deque
+input = sys.stdin.readline
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def bfs(x, y):                           #세로: 0 <= x < N  가로: 0 <= y < M
+    q = deque()     #([x,y])
+    q.append([x,y])
+    # q.append((x,y))
+    # print(q)
+    while q:
+        x,y = q.popleft()    #q.pop(0)
+       
+        for i in range(4):
+            xi = x + dx[i]
+            yi = y + dy[i]
+            if not (xi < 0 or xi >= N or yi < 0 or yi >= M):   # if (0 <= xi < N) and (0 <= yi < M):
+                if maze[xi][yi] == 1 and visited[xi][yi] == 0:
+                    q.append([xi,yi])
+                    maze[xi][yi] = maze[x][y]+1
+                    visited[xi][yi] = 0 
+    print(maze[N-1][M-1])
+  
+
+N, M = map(int, input().split())
+maze = []
+for i in range(N):
+    maze.append(list(map(int, input().strip()))) # 공백 제거 해줘야. readline의 경우 개행문자를 입력받기 때문. 
+# print(lst)
+visited = [[0]*M for i in range(N)]
+
+bfs(0,0)
+
